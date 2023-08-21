aa0a5d7f2dffaec6eec6fc334d8dbeed80dd8c1e
vishnuSekhar <vishnusekhar96@gmail.com>
2023-08-21 19:25:53 +0530

    fea:cert-one-complete

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GlobalStyles from './styles';
-import Pages from './pages';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
+import GlobalStyles from "./styles";
+import Pages from "./pages";
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <GlobalStyles />
     <Pages />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ApolloProvider>,
+  document.getElementById("root")
 );
--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,40 @@
-import React from 'react';
-import { Layout } from '../components';
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+import { Layout } from "../components";
+import TrackCard from "../containers/track-card";
+import QueryResult from "../components/query-result";
 
+const TRACKS = gql`
+  query GetTracks {
+    tracksforHome {
+      title
+      id
+      thumbnail
+      length
+      modulesCount
+      author {
+        id
+        name
+        photo
+      }
+    }
+  }
+`;
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+  return (
+    <Layout grid>
+      <QueryResult error={error} loading={loading} data={data}>
+        {data?.tracksforHome?.map((track) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Tracks;
