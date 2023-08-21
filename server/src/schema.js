730be43c2267660b7d92b44571d0603826a9f8d7
vishnuSekhar <vishnusekhar96@gmail.com>
2023-08-21 08:31:32 +0530

    fea:add-graphql-schema-using-sdl

---
+++ server/src/schema.js
@@ -0,0 +1,25 @@
+const gql = require("graphql-tag");
+
+const typeDefs = gql`
+  type query {
+    tracksforHome: [Track!]!
+  }
+
+  "A track is a group of modules that teaches about a certain topic"
+  type Track {
+    id: ID!
+    title: String!
+    author: Author!
+    thumbnail: String
+    length: Int
+    modulesCount: Int
+  }
+  "Author of a complete track"
+  type Author {
+    id: ID!
+    name: String!
+    photo: String
+  }
+`;
+
+module.exports = typeDefs;
